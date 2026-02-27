557800c945f14b41e40c7c2eafb4382ef94dee5d
Zynith0 <nolan.lessard.music@gmail.com>
2026-02-27 13:53:36 -0500

    feat(header): personalized the header

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Zynith';
+export const LOGO = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Frosepinetheme.com%2Fassets%2Flogo-dawn.png&f=1&nofb=1&ipt=6f25c5d0a6f5c084a255c167c5edda6dd43adfed6f10f18887d90ae1dc17ccd5';
+export const SITE_DESCRIPTION = 'Welcome to my blog!';
